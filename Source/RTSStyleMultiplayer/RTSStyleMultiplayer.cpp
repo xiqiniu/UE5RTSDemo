2c5dd1278c890f7f78b5d3e6b385c216e078c04a
Li Xue <2506251615@qq.com>
2023-11-13 14:39:11 +0800

    first commit

---
+++ Source/RTSStyleMultiplayer/RTSStyleMultiplayer.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "RTSStyleMultiplayer.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, RTSStyleMultiplayer, "RTSStyleMultiplayer" );
+
+DEFINE_LOG_CATEGORY(LogRTSStyleMultiplayer)
+ 
